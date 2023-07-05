90201eb06bf953095a718f3b14a00a20248556e5
jirayu <jirayu.ka@logicspark.com>
2023-07-05 18:18:58 +0700

    add module role

--- src/permissions/features/entities/feature.entity.ts
+++ src/permissions/features/entities/feature.entity.ts
@@ -1,12 +1,19 @@
-import { ObjectType } from '@nestjs/graphql';
+import { ID, ObjectType } from '@nestjs/graphql';
 import { BasicData } from 'src/shared/dtos/basic-data.entity';
-import { Column, Entity } from 'typeorm';
-import { FilterableField } from '@nestjs-query/query-graphql';
+import { Column, Entity, JoinColumn, OneToOne, PrimaryColumn } from 'typeorm';
+import { FilterableField, IDField } from '@nestjs-query/query-graphql';
+import { Permission } from 'src/permissions/entities/permission.entity';
 @Entity('features', { schema: 'user' })
 @ObjectType({
   implements: () => [BasicData],
 })
 export class Feature extends BasicData {
+  @PrimaryColumn()
+  @IDField(() => ID, {
+    nullable: true,
+  })
+  id: number;
+
   @Column({ unique: false, nullable: false })
   @FilterableField({ nullable: true })
   name: string;
@@ -14,6 +21,10 @@ export class Feature extends BasicData {
   @Column({ nullable: false, type: 'varchar' })
   @FilterableField({ nullable: true })
   description: string;
+
+  @OneToOne(() => Permission, (permission) => permission.feature)
+  @JoinColumn({ name: 'id' })
+  permissions?: Permission;
 
   public constructor(partial: Partial<Feature>) {
     super();
