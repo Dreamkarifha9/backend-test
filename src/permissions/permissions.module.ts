a4c414d363397bfd2232751a80ca9a16123832c5
jirayu <jirayu.ka@logicspark.com>
2023-07-05 16:33:30 +0700

    create feature module

--- src/permissions/permissions.module.ts
+++ src/permissions/permissions.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PermissionsService } from './permissions.service';
 import { PermissionsResolver } from './permissions.resolver';
+import { FeaturesModule } from './features/features.module';
 
 @Module({
-  providers: [PermissionsResolver, PermissionsService]
+  providers: [PermissionsResolver, PermissionsService],
+  imports: [FeaturesModule]
 })
 export class PermissionsModule {}
