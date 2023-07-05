faa62ae61ea646c6f64537b476f64885e83f365b
jirayu <jirayu.ka@logicspark.com>
2023-07-05 16:36:13 +0700

    create feature.entity

--- src/permissions/features/entities/feature.entity.ts
+++ src/permissions/features/entities/feature.entity.ts
@@ -1,1 +1,22 @@
-export class Feature {}
+import { ObjectType } from '@nestjs/graphql';
+import { BasicData } from 'src/shared/dtos/basic-data.entity';
+import { Column, Entity } from 'typeorm';
+import { FilterableField } from '@nestjs-query/query-graphql';
+@Entity('features', { schema: 'user' })
+@ObjectType({
+  implements: () => [BasicData],
+})
+export class Feature extends BasicData {
+  @Column({ unique: false, nullable: false })
+  @FilterableField({ nullable: true })
+  name: string;
+
+  @Column({ nullable: false, type: 'varchar' })
+  @FilterableField({ nullable: true })
+  description: string;
+
+  public constructor(partial: Partial<Feature>) {
+    super();
+    Object.assign(this, partial);
+  }
+}
--- src/permissions/features/features.module.ts
+++ src/permissions/features/features.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { FeaturesService } from './features.service';
-import { FeaturesResolver } from './features.resolver';
+
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Feature } from './entities/feature.entity';
 
 @Module({
-  providers: [FeaturesResolver, FeaturesService]
+  imports: [TypeOrmModule.forFeature([Feature])],
 })
-export class FeaturesModule {}
+export class FeaturesModule { }
